7a234185278e19b9a94f5fb633fd2f366b69e74d
gonzalezalfie <60169495+gonzalezalfie@users.noreply.github.com>
2021-02-18 05:49:16 -0600

    Se ejecuta gestor_contrasenas.py con un /usr/bin/gestor_contrasenas.

--- instalador.sh
+++ instalador.sh
@@ -6,22 +6,11 @@ cd $HOME/gestor_contrasenas
 
 wget https://raw.githubusercontent.com/gonzalezalfie/gestor_contrasenas/main/gestor_contrasenas.py
 
-sudo chmod +x gestor_contrasenas.py
-
-#sudo apt-get install python3-pip
-
-#export LC_ALL=C
-
-sudo apt-get install python3-numpy
-
-sudo apt-get install python3-pandas
-
-sudo apt-get install python3-crypto
+chmod +x gestor_contrasenas.py
 
 sudo echo "python3 $HOME/gestor_contrasenas/gestor_contrasenas.py" > /usr/bin/gestor_contrasenas
 
 sudo chmod +x /usr/bin/gestor_contrasenas
 
 #echo "alias gestor_contrasenas='python3 $HOME/gestor_contrasenas/#gestor_contrasenas.py'" >> ~/.bashrc
-
 
