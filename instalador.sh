9ae05b590f442b2f394899981a011b636ef398e0
gonzalezalfie <60169495+gonzalezalfie@users.noreply.github.com>
2021-02-17 19:57:49 -0600

    Ya se puede usar pip.

--- instalador.sh
+++ instalador.sh
@@ -6,9 +6,9 @@ cd $HOME/gestor_contrasenas
 
 wget https://raw.githubusercontent.com/gonzalezalfie/gestor_contrasenas/main/gestor_contrasenas.py
 
-sudo apt update
+sudo apt-get update
 
-sudo apt install python3-pip
+sudo apt-get install python3-pip
 
 pip install numpy
 
@@ -18,6 +18,6 @@ pip install pycrypto
 
 sudo echo "python3 $HOME/gestor_contrasenas/gestor_contrasenas.py" > /usr/bin/gestor_contrasenas
 
-sudo chmod +x gestor_contrasenas
+sudo chmod +x gestor_contrasenas.py
 
 
