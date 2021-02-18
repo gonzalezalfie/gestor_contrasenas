beeca24f70b47d38594475076f5764f43de05ab5
gonzalezalfie <60169495+gonzalezalfie@users.noreply.github.com>
2021-02-17 20:09:49 -0600

    Se instala numpy y las otras librerías con sudo apt-get install.

--- instalador.sh
+++ instalador.sh
@@ -6,15 +6,11 @@ cd $HOME/gestor_contrasenas
 
 wget https://raw.githubusercontent.com/gonzalezalfie/gestor_contrasenas/main/gestor_contrasenas.py
 
-sudo apt-get update
+sudo apt-get install python3-numpy
 
-sudo apt-get install python3-pip
+sudo apt-get install python3-pandas
 
-pip install numpy
-
-pip install pandas
-
-pip install pycrypto
+sudo apt-get install python3-pycrypto
 
 sudo echo "python3 $HOME/gestor_contrasenas/gestor_contrasenas.py" > /usr/bin/gestor_contrasenas
 
